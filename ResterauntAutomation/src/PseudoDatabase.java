bf623681360d1b1e9c12389400e253716ebe4637
emmam95 <emmam95@users.noreply.github.com>
2016-04-20 18:25:39 -0400

    Added junit tests for database, minor database bug fixes

--- ResterauntAutomation/src/PseudoDatabase.java
+++ ResterauntAutomation/src/PseudoDatabase.java
@@ -4,28 +4,70 @@ public class PseudoDatabase
 {
     private Map<Integer, Employee> Map_ID_Employee;
 
+
     public PseudoDatabase()
     {
         Map_ID_Employee = new HashMap<Integer, Employee>();
-      //does anything need to be here? idk
     }
 
-    public void addEmployee(Employee bob){
-      Map_ID_Employee.put(bob.getEmployeeInfo().getEmployeeID(), bob);
+
+    public void addEmployee(Employee employee)
+    {
+        assert (checkForUniqueID(employee.getEmployeeInfo().getEmployeeID()));
+        Map_ID_Employee.put(
+            employee.getEmployeeInfo().getEmployeeID(),
+            employee);
     }
 
-    public void removeEmployee(Employee bob){
-      Map_ID_Employee.remove(bob.getEmployeeInfo().getEmployeeID());
+
+    public void removeEmployee(Employee employee)
+    {
+        if (!Map_ID_Employee.containsValue(employee))
+        {
+            // display to user that error occurred
+            System.out.println("Error, cannot remove this employee");
+            return;
+        }
+        Map_ID_Employee.remove(employee.getEmployeeInfo().getEmployeeID());
     }
 
-    public void removeEmployeeById(int id){
-      Map_ID_Employee.remove(id);
+
+    public void removeEmployeeById(int id)
+    {
+        if (!Map_ID_Employee.containsKey(id))
+        {
+            // display to user that error occurred
+            System.out.println("Error, cannot remove this employee");
+            return;
+        }
+        Map_ID_Employee.remove(id);
     }
 
-    //Will return the employee or NULL if there is no employee with that id
+
+    // Will return the employee or NULL if there is no employee with that id
     public Employee getEmployee(int id)
     {
-      return Map_ID_Employee.get(id);
+        if (Map_ID_Employee.get(id) == null)
+        {
+            System.out.println("Error, cannot retrieve an employee by this ID");
+        }
+        return Map_ID_Employee.get(id);
+    }
+
+
+    /**
+     * This method makes sure an ID entered is truly unique To be used for error
+     * checking in the pseudo interface Returns true if the ID is unique
+     */
+    public boolean checkForUniqueID(int id)
+    {
+        return !Map_ID_Employee.containsKey(id);
+    }
+
+
+    public Map<Integer, Employee> getMap()
+    {
+        return Map_ID_Employee;
     }
 
 }
