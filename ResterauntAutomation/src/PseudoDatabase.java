32440f4568dce4331fbf080068f06cd2a7f251c0
emmam95 <emmam95@users.noreply.github.com>
2016-04-20 15:29:27 -0400

    Added files via upload

--- ResterauntAutomation/src/PseudoDatabase.java
+++ ResterauntAutomation/src/PseudoDatabase.java
@@ -1,38 +1,31 @@
-import java.util.*
+import java.util.*;
 
 public class PseudoDatabase
 {
-    private HashMap<Integer, Employee> Map_ID_Employee;
-    
-    public PseudoDatabase(){
+    private Map<Integer, Employee> Map_ID_Employee;
+
+    public PseudoDatabase()
+    {
+        Map_ID_Employee = new HashMap<Integer, Employee>();
       //does anything need to be here? idk
     }
-    
+
     public void addEmployee(Employee bob){
-      Map_ID_Employee.put(bob.getEmpolyeeInfo().getEmployeeID(), bob);  
+      Map_ID_Employee.put(bob.getEmployeeInfo().getEmployeeID(), bob);
     }
-    
+
     public void removeEmployee(Employee bob){
       Map_ID_Employee.remove(bob.getEmployeeInfo().getEmployeeID());
     }
-    
+
     public void removeEmployeeById(int id){
       Map_ID_Employee.remove(id);
     }
-    
+
     //Will return the employee or NULL if there is no employee with that id
-    public Employee getEmployee(int id){
+    public Employee getEmployee(int id)
+    {
       return Map_ID_Employee.get(id);
     }
-    
-    public void printEmployees(){
-      for (key in Map_ID_Employee.keySet()){
-        EmployeeInfo info = Map_ID_Employee.get(key).getEmployeeInfo();
-        StringBuilder s = new StringBuilder(info.getEmployeeID());
-        s.append(" - ");
-        s.append(temp.getFirstName())
-        s.append(temp.getLastName());
-        System.out.println(s);
-      }
-    }
+
 }
